247068616d49c59d9805887dab656947983ab046
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2024-11-29 15:09:52 -0300

    Add files via upload

---
+++ aula1_prolog.pl
@@ -0,0 +1,70 @@
+mulher(ana).
+mulher(bia).
+mulher(eva).
+mulher(clo).
+mulher(lia).
+mulher(gal).
+
+homem(ivo).
+homem(gil).
+homem(rai).
+homem(ary).
+homem(noe).
+
+mae(ana, eva).
+mae(bia, rai).
+mae(bia, clo).
+mae(bia, ary).
+mae(eva, noe).
+mae(lia, gal).
+
+pai(ivo, eva).
+pai(gil, rai).
+pai(gil, clo).
+pai(gil, ary).
+pai(rai, noe).
+pai(ary, gal).
+
+gerou(X,Y) :- pai(X, Y) ; mae(X, Y).
+
+irmao(X, Y) :- gerou(Z, X), 
+    			gerou(Z, Y), 
+    			homem(X), 
+    			X \== Y.
+
+irma(X, Y) :- gerou(Z, X), 
+    			gerou(Z, Y), 
+    			mulher(X), 
+    			X \== Y.
+
+tio(X, Y) :- irmao(X, Z),
+    			gerou(Z, Y),
+    			homem(X).
+    
+tia(X, Y) :- irma(X, Z),
+    			gerou(Z, Y),
+    			mulher(X).
+
+avô(X,Y) :- pai(X,Z),
+    		gerou(Z,Y).
+    
+avó(X,Y) :- mae(X,Z),
+    		gerou(Z,Y).  
+    
+casal(X,Y) :- pai(X,Z), mae(Y,Z).
+    
+feliz(X) :- gerou(X, Y).
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
